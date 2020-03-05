235c3ec02e4d3cba5587d376e89dd31053478aa7
bayramcicek <mail@bayramcicek.com.tr>
2020-03-05 20:17:19 +0300

    insertion sort

--- 008-insertion-sort.py
+++ 008-insertion-sort.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3.6
 # created by cicek on Mar 03, 2020 14:52
 
-def my_insertion_sort(arr=[2, 1, 6, -2, 4, 9, -5]):
-    n = len(arr)
+
+def insertion_sort(list):
+    n = len(list)
     loop_counter = 0
+
     for i in range(1, n):
-        key = arr[i]
+        key = list[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
             j = j - 1
             loop_counter = loop_counter + 1
-        arr[j + 1] = key
-    return arr, loop_counter
+
+        list[j + 1] = key
+
+    return list, loop_counter
 
 
-arr = [2, 1, 6, -2, 4, 9, -5]
-print(arr)
-arr, l_c = my_insertion_sort()
-print(arr, l_c)
+list = [2, 1, 6, -2, 4, 9, -5]
 
-# [2, 1, 6, -2, 4, 9, -5] --> sıralı gönder 11 -> 0 olur
-# [-5, -2, 1, 2, 4, 6, 9] 11 --> iterasyon sayısı
+list, loop_counter = insertion_sort(list)
+print(list, "\ndöngü:", loop_counter)
+
+# [2, 1, 6, -2, 4, 9, -5] --> sıralı gönder,  döngü: 11 -> döngü: 0 olur
