80f343ec4e9ae6d454d311b6f9687fcd3a8e9b6f
bayramcicek <mail@bayramcicek.com.tr>
2020-04-04 21:24:30 +0300

    taslak eklendi

--- 011-odev-1-mod-medyan.py
+++ 011-odev-1-mod-medyan.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3.6
 # created by cicek on Apr 04, 2020 20:14
 
+import random
 
 '''
-içerik adı : ikili arama
-yayın : 21 Mart 2020
-
-ikili arama ve medyan bulma algoritmalarını gönderiyorum
-kaynak geekforgeek üzerinden de açıklamaları okuyup kodları çalıştırabilirsiniz.
-
-- karmaşıklık binary search için O(log n) 'dir
-- best case O(1)
-- median için sıralama yapılması veya frekans tablosundan bulunması gerekliliğinden karmaşıklığı fazladır
-'''
-
-'''
-ödev 1 mode medyan karmaşıklığı
+- ödev 1 mode medyan karmaşıklığı
 Due tomorrow at 11:59 PM
-Instructions
-mode ve medyanın her ikisini return edecek şekilde bir fonksiyon yazınız. Fonksiyon içerisinden başka bir fonksiyona çağrım yapılmayacaktır.
+Instructions: mode ve medyanın her ikisini return edecek şekilde bir fonksiyon yazınız. Fonksiyon içerisinden 
+              başka bir fonksiyona çağrım yapılmayacaktır.
 
 import random
 def getModeMedian(list_1):
@@ -29,5 +18,5 @@ def getModeMedian(list_1):
    ...
 
    return mode,median
-
 '''
+
