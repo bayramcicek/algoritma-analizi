42fcdcf87260503d56e7346936ff035c11d89ed8
bayramcicek <mail@bayramcicek.com.tr>
2020-06-10 11:01:46 +0300

    varyans

---
+++ 017-0-hafta-8-standard-deviation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# created by cicek on Jun 10, 2020 07:07
+
+'''
+bu konuda final sınavı dair şöyle bir soru olacak:
+
+bir liste üzerinde linear ve binary arama:
+m boyutlu liste için n tane  örneklem üretip,
+ortalama ve varsans için grafiğini çizip sonuçlarını
+linear-binary search karmaşıklığını dikkate alarak yorumlayınız.
+
+hata -> varyans'a bağlıdır.
+varyans arttıkça daha fazla sample almak gerekir. çünkü
+hata artmış demektir.
+standart sapma = karekök(varyans)
+
+Central Limit Theorem:
+sample varyans ile tüm sample varyans'ı birbirine yakındır.
+sample ortalaması ile tüm sample ortalaması birbirine yakındır.
+
+'''
---
+++ 017-1-hafta-8-means-of-samples.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# created by cicek on Jun 10, 2020 11:00
+
